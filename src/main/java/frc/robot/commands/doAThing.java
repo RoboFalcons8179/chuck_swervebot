08d67fe51472f8e35c7aeac8dacb59ed2e7ab92c
My Name <thesame@email.com>
2023-02-27 20:02:35 -0500

    removed redundant V2s

--- src/main/java/frc/robot/commands/doAThing.java
+++ src/main/java/frc/robot/commands/doAThing.java
@@ -6,12 +6,15 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class doAThing implements Command {
-   public Void thing; {
-    System.out.println("test");
-   }
-@Override
-public Set<Subsystem> getRequirements() {
-    // TODO Auto-generated method stub
-    return null;
-}
+
+    public doAThing() {
+        System.out.println("*******,,,,,,,,,,,,,\n,***,,,,,........,,,\n,,,,,,,,(#.....//.,,\n,,,,,,,,/,*.(*/((,,,\n*,,,,,,*#*##*,#,,,,,\n,,,,**/&%(*(,,,/((,,\n*,,,,,*/##/,,,,,,,,,\n***********,,,,,,,,,");
+    }
+
+    @Override
+    public Set<Subsystem> getRequirements() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
 }
