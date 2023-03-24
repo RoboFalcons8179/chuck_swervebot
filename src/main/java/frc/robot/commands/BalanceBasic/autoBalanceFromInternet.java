27c22e8652ecff1d63100ae8054cc6507f3625a3
My Name <thesame@email.com>
2023-03-23 22:21:12 -0400

    credit

--- src/main/java/frc/robot/commands/BalanceBasic/autoBalanceFromInternet.java
+++ src/main/java/frc/robot/commands/BalanceBasic/autoBalanceFromInternet.java
@@ -2,6 +2,8 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+// Thanks to team 3683 !!!!
+
 package frc.robot.commands.BalanceBasic;
 
 import edu.wpi.first.math.geometry.Rotation2d;
